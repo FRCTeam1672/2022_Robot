d3edc402f621de2883c35163449111081d57299a
Joey <23sirj01@mahwah.k12.nj.us>
2022-02-14 17:37:14 -0500

    Replaced VictorSP Test Motor Controllers With TalonSRX & VictorSPX Controllers & Added Motor Controller DeviceID Constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveSubsystem m_exampleSubsystem = new DriveSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
@@ -30,9 +30,9 @@ public class RobotContainer {
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {}
 
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,22 +1,39 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Drive;
 
 public class DriveSubsystem extends SubsystemBase {
-  private VictorSP left;
-  private VictorSP right;
+  private WPI_VictorSPX backLeft;
+  private WPI_VictorSPX backRight;
+
+  private WPI_TalonSRX frontLeft;
+  private WPI_TalonSRX frontRight;
+
   private DifferentialDrive drive;
+
   private XboxController xController;
   // private PIDController pidController;
 
   public DriveSubsystem() {
-    this.left = new VictorSP(0);
-    this.right = new VictorSP(1);
-    this.drive = new DifferentialDrive(left, right);
+    this.frontLeft = new WPI_TalonSRX(Drive.FRONT_LEFT);
+    this.frontRight = new WPI_TalonSRX(Drive.FRONT_RIGHT);
+
+    this.backLeft = new WPI_VictorSPX(Drive.BACK_LEFT);
+    this.backRight = new WPI_VictorSPX(Drive.BACK_RIGHT);
+
+    this.frontRight.setInverted(true);
+    this.backRight.setInverted(true);
+
+    this.backLeft.follow(this.frontLeft);
+    this.backRight.follow(this.frontRight);
+
+    this.drive = new DifferentialDrive(frontLeft, frontRight);
     this.xController = new XboxController(0);
 
     // Constructor W/out Period Also Exists
