140ee8507986d8a8660d99049a6d0152448bda60
Kiera <kierajreed@gmail.com>
2022-03-18 16:22:58 -0400

    add new climb commands; clean up ClimbSubsystem

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -3,14 +3,8 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Climb;
-import frc.robot.commands.climb.ExtendArmsCommand;
-import frc.robot.commands.climb.ExtendCenterArmCommand;
-import frc.robot.commands.climb.RetractInnerArmCommand;
-import frc.robot.commands.climb.RetractOuterArmsCommand;
-import frc.robot.commands.climb.UnlockArmsCommand;
 
 public class ClimbSubsystem extends SubsystemBase {
     private WPI_TalonSRX left;
@@ -19,15 +13,6 @@ public class ClimbSubsystem extends SubsystemBase {
 
     private Solenoid solenoid;
     private Solenoid centerSolenoid;
-
-    private CommandBase[] commands = {new ExtendArmsCommand(this),
-            new RetractOuterArmsCommand(this), new ExtendCenterArmCommand(this),
-            new RetractInnerArmCommand(this), new UnlockArmsCommand(this)};
-    private int nextCommand = 0;
-    private final int MID_CLIMB_MAX = 1;
-
-    private boolean running = false;
-    private CommandBase current = commands[0];
 
     public ClimbSubsystem() {
         this.left = new WPI_TalonSRX(Climb.LEFT);
@@ -58,27 +43,5 @@ public class ClimbSubsystem extends SubsystemBase {
 
     public Solenoid getCenterSolenoid() {
         return centerSolenoid;
-    }
-
-    public CommandBase nextCommand(boolean highClimb) {
-        if (!highClimb && nextCommand > MID_CLIMB_MAX)
-            return null;
-
-        return commands[nextCommand++];
-    }
-
-    public void runNextCommand(boolean high) {
-        running = true;
-        current = nextCommand(high);
-    }
-
-    @Override
-    public void periodic() {
-        if (running) {
-            if (!current.isFinished())
-                current.execute();
-
-            running = !current.isFinished();
-        }
     }
 }
